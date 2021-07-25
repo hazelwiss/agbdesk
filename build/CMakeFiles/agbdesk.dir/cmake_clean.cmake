2bee46ed3f1a812e2f6d534992d583c5fa6dcea2
Kim-Dewelski <edwin.ljung@gmail.com>
2021-07-25 21:56:55 +0200

    initial commit

---
+++ build/CMakeFiles/agbdesk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/agbdesk"
+  "../bin/agbdesk.pdb"
+  "CMakeFiles/agbdesk.dir/src/main.cpp.o"
+  "CMakeFiles/agbdesk.dir/src/test/test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agbdesk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
